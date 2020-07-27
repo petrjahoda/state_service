97fbc4a84b574d99726fe20467e08c3b69f76fa7
Petr Jahoda <petrjahoda@protonmail.com>
2020-07-27 10:14:06 +0200

    ## [2020.3.1.26] - 2020-07-26
    ### Changed
    - changed to gorm v2
    - postgres only
    - simlke logging
    
    ### Removed
    - logging to file
    - all about config file
    
    Signed-off-by: Petr Jahoda <petrjahoda@protonmail.com>
    
    Took 3 minutes

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 	"time"
 )
 
-const version = "2020.3.1.26"
+const version = "2020.3.1.27"
 const programName = "State Service"
 const programDesription = "Creates states for workplaces"
 const downloadInSeconds = 10
