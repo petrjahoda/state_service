4ba0ee45fc4f1620ea1b3ecdad3df2cff3eac9fd
Petr Jahoda <petrjahoda@protonmail.com>
2020-08-22 12:19:30 +0200

    ## [2020.3.2.22] - 2020-08-22
    ### Added
    - automatic go get -u all when creating docker image
    
    Signed-off-by: Petr Jahoda <petrjahoda@protonmail.com>
    
    Took 1 minute

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 	"time"
 )
 
-const version = "2020.3.2.13"
+const version = "2020.3.2.22"
 const serviceName = "State Service"
 const serviceDescription = "Creates states for workplaces"
 const downloadInSeconds = 10
